5abd1c6b0ed5a703a715228fb40a07b804754408
takiding <takiding@tencent.com>
2021-09-06 16:49:51 +0800

    基础项目
    1、平台切到安卓
    2、Assets/Scripts/Editor/JenkinsAdapter.cs是自动打包关联脚本，勿动

---
+++ CatClub/Assets/Scripts/Editor/JenkinsAdapter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class JenkinsAdapter
+{
+    [MenuItem("Jenkins/JenkinsBuild")]
+    public static void Build()
+    {
+        Debug.Log("HOMEDRIVE = " + System.Environment.GetEnvironmentVariable("HOMEDRIVE"));
+        Debug.Log("HOMEPATH = " + System.Environment.GetEnvironmentVariable("HOMEPATH"));
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(targetGroup: BuildTargetGroup.Android, target: BuildTarget.Android);
+
+        List<string> sceneList = new List<string>();
+        EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
+        for (int i = 0, iMax = temp.Length; i < iMax; ++i)
+            sceneList.Add(temp[i].path);
+        DateTime date3 = DateTime.Now;
+        string timeInfo = date3.ToString("yyyy.MM.dd_HH.mm.ss");
+
+        BuildPipeline.BuildPlayer(sceneList.ToArray(), "E:/MiniGameBuildOutputs/CatClub_" + timeInfo + ".apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
