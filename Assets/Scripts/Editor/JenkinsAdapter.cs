740f6cd2f9408a810dc7d8425555ed16e245cb90
takiding <takiding@tencent.com>
2021-09-07 10:30:15 +0800

    Revert "调整打包脚本"
    This reverts commit a115cfacf51217bb00e350dc638d9a808dc56afe.

--- Assets/Scripts/Editor/JenkinsAdapter.cs
+++ Assets/Scripts/Editor/JenkinsAdapter.cs
@@ -17,10 +17,9 @@ public class JenkinsAdapter
         EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
         for (int i = 0, iMax = temp.Length; i < iMax; ++i)
             sceneList.Add(temp[i].path);
-        //DateTime date3 = DateTime.Now;
-        //string timeInfo = date3.ToString("yyyy.MM.dd_HH.mm.ss");
+        DateTime date3 = DateTime.Now;
+        string timeInfo = date3.ToString("yyyy.MM.dd_HH.mm.ss");
 
-        //BuildPipeline.BuildPlayer(sceneList.ToArray(), "E:/MiniGameBuildOutputs/CatClub_" + timeInfo + ".apk", BuildTarget.Android, BuildOptions.None);
-        BuildPipeline.BuildPlayer(sceneList.ToArray(), "E:/MiniGameBuildOutputs/CatClub.apk", BuildTarget.Android, BuildOptions.None);
+        BuildPipeline.BuildPlayer(sceneList.ToArray(), "E:/MiniGameBuildOutputs/CatClub_" + timeInfo + ".apk", BuildTarget.Android, BuildOptions.None);
     }
 }
