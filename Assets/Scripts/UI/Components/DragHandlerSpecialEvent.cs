e0c6a3d78dce219c3f886dd6b22cd0aa3c70e1a2
takiding <takiding@tencent.com>
2021-09-09 18:48:07 +0800

    UI斜向拖出

--- Assets/Scripts/UI/Components/DragHandlerSpecialEvent.cs
+++ Assets/Scripts/UI/Components/DragHandlerSpecialEvent.cs
@@ -3,47 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using FitMode = UnityEngine.UI.ContentSizeFitter.FitMode;
 
-public class DragHandlerSpecialEvent : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragHandlerSpecialEvent : MonoBehaviour,
+    IBeginDragHandler,
+    IDragHandler,
+    IDropHandler,
+    IEndDragHandler
+    
 {
     
     private RectTransform rectTransform;
-    private GameObject DraggingImage;
+    private GameObject draggingImage;
+    private ScrollRect scrollRect;
     
     private Vector3 pos;                            //控件初始位置
     private Vector3 mousePos;                       //鼠标初始位置
 
+    public float extractAngle;
+    private bool bIsExtracting;
+    private uint timerDragStart;
+    private float tempMousePos_x;
+
+    void Start()
+    {
+        scrollRect = GameObject.Find("ScrollSpecialEvent").GetComponent<ScrollRect>();
+    }
     private GameObject InsImage()
     {
         GameObject tempImg = Instantiate(gameObject);
-        tempImg.transform.SetParent(transform.parent.parent.parent.parent,false);
+        tempImg.transform.SetParent(scrollRect.transform,false);
         return tempImg;
     }
     
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("开始拖拽");
-        DraggingImage = InsImage();
-        transform.Find("ImageEventEmpty").gameObject.SetActive(true);
-        rectTransform = DraggingImage.transform.GetComponent<RectTransform>();
-        pos = GetComponent<RectTransform>().position;    
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out mousePos);
+        bIsExtracting = false;
+        timerDragStart = 6;
+        draggingImage = InsImage();
+        rectTransform = draggingImage.transform.GetComponent<RectTransform>();
+        draggingImage.SetActive(false);
+        
+        pos = GetComponent<RectTransform>().position;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, null, out mousePos);
+        tempMousePos_x = mousePos.x;
     }
     
     public void OnDrag(PointerEventData eventData)
     {
         Vector3 newVec;          
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out newVec);
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, null, out newVec);
         Vector3 offset = new Vector3(newVec.x - mousePos.x, newVec.y - mousePos.y, 0);
         rectTransform.position = pos + offset;
- 
+        if (timerDragStart > 1)
+        {
+            timerDragStart--;
+            
+        }
+        if (timerDragStart == 2)
+        {
+            if (offset.y > 0 && offset.y/Mathf.Abs(offset.x) >= Mathf.Sin(extractAngle * Mathf.Deg2Rad))
+            {
+                bIsExtracting = true;
+                draggingImage.SetActive(true);
+                transform.Find("ImageEventEmpty").gameObject.SetActive(true);
+            }
+        }
+        
+        if (!bIsExtracting)
+        {
+            rectTransform.position = pos;
+            if (timerDragStart == 1)
+            {
+                draggingImage.SetActive(false);
+                transform.Find("ImageEventEmpty").gameObject.SetActive(false);
+            }
+            print("pos = "+scrollRect.horizontalNormalizedPosition+"\ndelta = "+(Input.mousePosition.x - tempMousePos_x) * 10.0f / (HandleSelfFittingHorizontal(transform.parent.parent)));
+            scrollRect.horizontalNormalizedPosition -= (Input.mousePosition.x - tempMousePos_x) * Screen.width * 1.7f / Mathf.Pow(HandleSelfFittingHorizontal(transform.parent.parent),2.0f);
+            tempMousePos_x = Input.mousePosition.x;
+        }
+
     }
-    
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+    }
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("结束拖拽");
-        GameObject.Destroy(DraggingImage);
+        bIsExtracting = false;
+        timerDragStart = 6;
+        Destroy(draggingImage);
         transform.Find("ImageEventEmpty").gameObject.SetActive(false);
     }
- 
+    
+    private float HandleSelfFittingHorizontal(Transform obj)
+    {
+        FitMode fitting = obj.GetComponent<ContentSizeFitter>().horizontalFit;
+        if (fitting == FitMode.MinSize)
+        {
+            return LayoutUtility.GetMinSize(obj.GetComponent<RectTransform>(), 0);
+        }
+        else
+        {
+            return LayoutUtility.GetPreferredSize(obj.GetComponent<RectTransform>(), 0);
+        }
+    }
+
 }
