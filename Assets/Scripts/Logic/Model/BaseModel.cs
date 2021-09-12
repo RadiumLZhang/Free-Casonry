bd13873637ed939a69a92338c5476719efb697ab
xinqizhou <xinqizho@usc.edu>
2021-09-12 19:42:14 +0800

    新增一些数据类

---
+++ Assets/Scripts/Logic/Model/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Logic
+{
+    public class BaseModel<T> where T : new()
+    {
+
+        private static T m_instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = new T();
+                }
+
+                return m_instance;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Logic/Model/CatModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Logic
+{
+    public class CatModel : BaseModel<CatModel>
+    {
+        private Dictionary<long, Cat> m_catDic;
+
+        public Cat GetCatByID(long id)
+        {
+            if(!m_catDic.TryGetValue(id, out var cat))
+            {
+                cat = new Cat(id);
+                m_catDic[id] = cat;
+            }
+
+            return cat;
+        }
+    }
+}
