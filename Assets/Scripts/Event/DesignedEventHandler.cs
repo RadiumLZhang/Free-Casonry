afe7a657db5a874a4175324a499b6b88baf0e6db
muidarzhang(张镭) <muidarzhang@tencent.com>
2021-09-13 10:22:00 +0800

    start event handler work

---
+++ Assets/Scripts/Event/DesignedEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Net.Security;
+
+namespace Event
+{
+    public class DesignedEventHandler:IComparable<DesignedEventHandler>
+    {
+        public catInfo catInfo = null;
+        public EventInfo eventInfo = null;
+        public int cacheTime = 0;
+        
+        // constructor
+        private DesignedEventHandler(CatInfo cat)
+        {
+            catInfo = cat;
+        }
+        
+        private DesignedEventHandler(CatInfo catIns, EventInfo eventIns)
+        {
+            catInfo = catIns;
+            eventInfo = eventIns;
+        }
+
+        public void callback()
+        {
+            
+        }
+        
+        
+        
+    }
+}
