b1fb16d5ce67d1085cbceb0fffecc6380b43c294
takiding <takiding@tencent.com>
2021-09-15 00:11:43 +0800

    猫咪Handler栏位有事件时，禁止接收新事件拖入

--- Assets/Scripts/UI/Components/CatColumnHandler.cs
+++ Assets/Scripts/UI/Components/CatColumnHandler.cs
@@ -19,25 +19,23 @@ public class CatColumnHandler : MonoBehaviour, IDropHandler
 
     public void OnDrop(PointerEventData eventData)
     {
-        //TODO:为你的猫咪Handler注册事件
         droppedEvent = eventData.pointerDrag.GetComponent<DragHandlerSpecialEvent>();
         myID = droppedEvent.GetEventID();
         print("what is index over here" + index);
         DesignedEventHandler eventHandler = EventManager.GetInstance().GetHandlerByID(index);
         eventHandler.SetEventInfo((int) myID);
-
-        //TODO:demo里不读表的话就自己写下myEventInfo结构体，有ID,Name,ConsumeTime和sprite即可
+        
         myEventInfo = eventHandler.GetEventInfo();
         transform.Find("ImageEvent").GetComponent<Image>().sprite = Resources.Load<Sprite>(myEventInfo.Image);
         transform.Find("ImageEvent").GetComponent<Image>().enabled = true;
         transform.Find("ImageEvent").Find("EventTimeBackground").gameObject.SetActive(true);
 
-        //用表中的ConsumeTime(需要消耗的时间)初始化remainingTime
         remainingTime = eventHandler.GetTimeRemain();
         transform.Find("ImageEvent").Find("EventTimeBackground").Find("TextEventTime").GetComponent<Text>().text =
             Convert.ToString(remainingTime) + "秒";
         droppedEvent.EndDrag();
         Destroy(droppedEvent.transform.parent.gameObject);
+        transform.GetComponent<Image>().raycastTarget = false;
     }
 
     public static void SendEventDestroyEvent(long id)
@@ -61,12 +59,11 @@ public class CatColumnHandler : MonoBehaviour, IDropHandler
     //TODO:事件完成时调这个方法清空栏位，可以是(0 == remainingTime)的时候？
     public void OnFinish()
     {
-        //TODO:用猫咪Handler调事件完成方法（影响议会资源之类的）
-        //清空UI的栏位并重设当前ID为-1
         transform.Find("ImageEvent").GetComponent<Image>().enabled = false;
         transform.Find("ImageEvent").GetComponent<Image>().sprite = null;
         transform.Find("ImageEvent").Find("EventTimeBackground").gameObject.SetActive(false);
         myID = -1;
+        transform.GetComponent<Image>().raycastTarget = true;
     }
 }
     
