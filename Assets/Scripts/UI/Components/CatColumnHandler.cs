508726fdf8da28a7d01ba5430747905c5dd70789
takiding <takiding@tencent.com>
2021-09-16 10:16:55 +0800

    处理中事件的倒计时显示改到猫头下方的绶带

--- Assets/Scripts/UI/Components/CatColumnHandler.cs
+++ Assets/Scripts/UI/Components/CatColumnHandler.cs
@@ -17,22 +17,29 @@ public class CatColumnHandler : MonoBehaviour, IDropHandler
     //remainingTime维护剩余时间
     private long remainingTime;
 
+    private Transform imageRemainingTime;
+    private Text textRemainingTime;
+    
+    void Start()
+    {
+        imageRemainingTime = transform.Find("CatPortrait").Find("ImageRemainingTime");
+        textRemainingTime = imageRemainingTime.Find("TextRemainingTime").GetComponent<Text>();
+    }
     public void OnDrop(PointerEventData eventData)
     {
         droppedEvent = eventData.pointerDrag.GetComponent<DragHandlerSpecialEvent>();
         myID = droppedEvent.GetEventID();
-        print("what is index over here" + index);
         DesignedEventHandler eventHandler = EventManager.GetInstance().GetHandlerByID(index);
         eventHandler.SetEventInfo((int) myID);
         
         myEventInfo = eventHandler.GetEventInfo();
         transform.Find("ImageEvent").GetComponent<Image>().sprite = Resources.Load<Sprite>(myEventInfo.Image);
         transform.Find("ImageEvent").GetComponent<Image>().enabled = true;
-        transform.Find("ImageEvent").Find("EventTimeBackground").gameObject.SetActive(true);
-
+        
         remainingTime = eventHandler.GetTimeRemain();
-        transform.Find("ImageEvent").Find("EventTimeBackground").Find("TextEventTime").GetComponent<Text>().text =
-            Convert.ToString(remainingTime) + "秒";
+        textRemainingTime.text = Convert.ToString(remainingTime) + "s";
+        imageRemainingTime.gameObject.SetActive(true);
+        
         droppedEvent.EndDrag();
         Destroy(droppedEvent.transform.parent.gameObject);
         transform.GetComponent<Image>().raycastTarget = false;
@@ -49,8 +56,7 @@ public class CatColumnHandler : MonoBehaviour, IDropHandler
         remainingTime = eventHandler.GetTimeRemain();
         //只要栏位中有事件(即ID不是默认的-1)就用remainingTime刷新UI显示
         if (myID != -1)
-            transform.Find("ImageEvent").Find("EventTimeBackground").Find("TextEventTime").GetComponent<Text>().text =
-                Convert.ToString(remainingTime) + "秒";
+            textRemainingTime.text = Convert.ToString(remainingTime) + "s";
         if (remainingTime == 0)
             OnFinish();
         
@@ -61,7 +67,7 @@ public class CatColumnHandler : MonoBehaviour, IDropHandler
     {
         transform.Find("ImageEvent").GetComponent<Image>().enabled = false;
         transform.Find("ImageEvent").GetComponent<Image>().sprite = null;
-        transform.Find("ImageEvent").Find("EventTimeBackground").gameObject.SetActive(false);
+        imageRemainingTime.gameObject.SetActive(false);
         myID = -1;
         transform.GetComponent<Image>().raycastTarget = true;
     }
