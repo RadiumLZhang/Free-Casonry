f84844318ebe10116788d26085e5867de726fe05
xinqizhou <xinqizho@usc.edu>
2021-09-24 19:46:06 +0800

    增加refreshOnPlay功能

---
+++ Assets/Scripts/Editor/RefreshOnPlay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public class RefreshOnPlay
+    {
+        static RefreshOnPlay()
+        {
+            EditorApplication.playModeStateChanged += OnRefreshOnPlay;
+        }
+
+        private static void OnRefreshOnPlay(PlayModeStateChange obj)
+        {
+            if (obj == PlayModeStateChange.ExitingEditMode)
+            {
+                //运行前更新代码和资源
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
