75633c85ccabe6d5696bbc06d2eaa8176d907480
Radium <muidarzhang@tencent.com>
2021-09-28 14:37:26 +0800

    change event status when event begin

--- Assets/Scripts/UI/Views/GameView.cs
+++ Assets/Scripts/UI/Views/GameView.cs
@@ -180,6 +180,7 @@ public class GameView : MonoBehaviour
         for (var i = 0; i < n; i++)
         {
             contentTransform.GetChild(i).GetComponent<SpecialEventMono>().InitWithID(events[i].ID);
+            events[i].Status = EventStatus.OnProcess;
         }
 
         //增加新增的
