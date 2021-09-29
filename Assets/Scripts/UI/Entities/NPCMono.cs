33959808ee4a2e519fb870826f06a550e4697e41
xinqizhou <xinqizho@usc.edu>
2021-09-29 16:18:43 +0800

    事件环动画

--- Assets/Scripts/UI/Entities/NPCMono.cs
+++ Assets/Scripts/UI/Entities/NPCMono.cs
@@ -14,16 +14,21 @@ public class NPCMono : MonoBehaviour
     private ScrollRect scrollRect;
     private NPCManager manager;
     private EventTrigger m_trigger;
+    private Animation m_animation;
+
     public List<NPCEventMono> eventcols = new List<NPCEventMono>();
+    
     private void Start()
     {
-        eventCycle = transform.Find("EventCycle").gameObject;
+        eventCycle = transform.Find("EventCycle/root").gameObject;
         selfRect = transform.GetComponent<RectTransform>();
         contentRect = GameObject.Find("ScrollRelationship").transform.Find("Viewport").Find("Content").GetComponent<RectTransform>();
         viewRect = GameObject.Find("ScrollRelationship").transform.Find("Viewport").GetComponent<RectTransform>();
         scrollRect = GameObject.Find("ScrollRelationship").GetComponent<ScrollRect>();
         manager = GameObject.Find("ScrollRelationship").GetComponent<NPCManager>();
         m_trigger = transform.GetComponent<EventTrigger>();
+        m_animation = transform.Find("EventCycle").GetComponent<Animation>();
+        m_animation.Stop();
         
         for (int i = 0; i < 5; i++)
         {
@@ -36,18 +41,55 @@ public class NPCMono : MonoBehaviour
         if (!eventCycle.activeSelf)
         {
             if (manager.currentOpenedNPC)
-                manager.currentOpenedNPC.transform.Find("EventCycle").gameObject.SetActive(false);
+            {
+                manager.currentOpenedNPC.GetComponent<NPCMono>().CloseEventCycle();
+            }
+            
             float contentX = contentRect.sizeDelta.x - viewRect.sizeDelta.x;
             float contentY = contentRect.sizeDelta.y - viewRect.sizeDelta.y;
             manager.StartNPCLerp((selfRect.anchoredPosition.x + contentX / 2.0f) / contentX,(selfRect.anchoredPosition.y + contentY / 2.0f) / contentY - 0.03f);
             manager.SetOpenedNPC(gameObject);
         }
-        eventCycle.SetActive(!eventCycle.activeSelf);
+        
+        var inAni = m_animation["EventCycleShow"];
+        if (eventCycle.activeSelf)
+        {
+            CloseEventCycle();
+        }
+        else
+        {
+            OpenEventCycle();
+        }
+        m_animation.Play("EventCycleShow");
+        
+        // eventCycle.SetActive(!eventCycle.activeSelf);
+    }
+
+    public void OpenEventCycle()
+    {
+        var inAni = m_animation["EventCycleShow"];
+        inAni.speed = 1;
+        inAni.normalizedTime = 0;
+        
+        inAni.enabled = false;
+        m_animation.Sample();
+        m_animation.Play("EventCycleShow");
+    }
+    
+    public void CloseEventCycle()
+    {
+        var inAni = m_animation["EventCycleShow"];
+        inAni.speed = -1;
+        inAni.normalizedTime = 1;
+        
+        inAni.enabled = false;
+        m_animation.Sample();
+        m_animation.Play("EventCycleShow");
     }
 
     public void ClostBtn_OnClick()
     {
-        eventCycle.SetActive(false);
+        // eventCycle.SetActive(false);
     }
 
     public void SetEventTriggerActive(bool active)
