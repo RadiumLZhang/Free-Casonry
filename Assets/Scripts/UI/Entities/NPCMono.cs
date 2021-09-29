beee5bbb74947ff910b04d680b8b3aa16840b4cc
takiding <takiding@tencent.com>
2021-09-29 17:17:09 +0800

    事件环路径与scene中对齐

--- Assets/Scripts/UI/Entities/NPCMono.cs
+++ Assets/Scripts/UI/Entities/NPCMono.cs
@@ -32,7 +32,7 @@ public class NPCMono : MonoBehaviour
         
         for (int i = 0; i < 5; i++)
         {
-            eventcols.Add(transform.Find("EventCycle").Find("NPCEvent" + i).GetComponent<NPCEventMono>());
+            eventcols.Add(transform.Find("EventCycle/root/NPCEvent" + i).GetComponent<NPCEventMono>());
         }
     }
 
