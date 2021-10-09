ff95d00f074d3b80dee56c293dda9e30033b45e9
takiding <takiding@tencent.com>
2021-10-09 22:14:34 +0800

    开场动画跳过

--- Assets/Scripts/UI/Views/StartMenuView.cs
+++ Assets/Scripts/UI/Views/StartMenuView.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using Manager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,19 +12,35 @@ public class StartMenuView : MonoBehaviour
     public string UserName;
     public Text TextUserName;
     public VideoPlayer OPPlayer;
+    private float TimerSkipOP;
+    private GameObject ButtonSkipOP;
     void Awake()
     {
         Input.multiTouchEnabled = true;
         TextUserName = transform.Find("InputField/Text").GetComponent<Text>();
         OPPlayer = transform.Find("OPPlayer").GetComponent<VideoPlayer>();
+        ButtonSkipOP = transform.Find("ButtonSkipOP").gameObject;
     }
-    
+
+    void Update()
+    {
+        if (TimerSkipOP > 0)
+        {
+            TimerSkipOP -= Time.deltaTime;
+            ButtonSkipOP.SetActive(true);
+        }
+        else
+        {
+            TimerSkipOP = 0;
+            ButtonSkipOP.SetActive(false);
+        }
+    }
     public void ButtonStart_OnClick()
     {
         UserName = TextUserName.text;
         OPPlayer.gameObject.SetActive(true);
         OPPlayer.Play();
-        OPPlayer.loopPointReached += StartGame;
+        OPPlayer.loopPointReached += OPFinished;
     }
     public void ButtonContinue_OnClick()
     {
@@ -32,9 +49,23 @@ public class StartMenuView : MonoBehaviour
         PlayerPrefs.SetString("saveName", UserName);
         SceneManager.LoadScene("Game");
     }
-
-    public void StartGame(VideoPlayer videoPlayer)
+    
+    public void ButtonOP_OnClick()
     {
+        TimerSkipOP = 3.5f;
+    }
+    public void ButtonSkipOP_OnClick()
+    {
+        OPPlayer.Stop();
+        StartGame();
+    }
+    public void OPFinished(VideoPlayer videoPlayer)
+    {
+        StartGame();
+    }
+    private void StartGame()
+    {
+        OPPlayer.gameObject.SetActive(false);
         PlayerPrefs.SetString("userName", UserName);
         PlayerPrefs.SetString("saveName", "");
         SceneManager.LoadScene("Game");
